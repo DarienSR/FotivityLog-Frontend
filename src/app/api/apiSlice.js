89a5cc13f223475fbf442291ad3ff1795d09517a
DarienSR <dariensloan@gmail.com>
2023-04-11 15:07:25 -0400

    url change

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../components/authentication/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_URL,
+    baseUrl: 'fotivitylog-backend-q2u9455i4-dariensr.vercel.app',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
