d67173613ba153302096e69039aceb9a07b56654
DarienSR <dariensloan@gmail.com>
2023-05-22 10:55:11 -0400

    2/2 fixing directory rename

---
+++ src/components/task/EditTask.js
@@ -0,0 +1,102 @@
+import { useState } from "react"
+import { useUpdateTaskMutation, useDeleteTaskMutation } from "./taskApiSlice"
+import { Link, useNavigate, useLocation } from "react-router-dom"
+import useAuth from '../../hooks/useAuth.js'
+export default function EditTask(props) {
+  const { user_id } = useAuth()
+
+  const navigate = useNavigate()
+
+  const [task, setTask] = useState(props.item.task)
+
+  const [updateTask] = useUpdateTaskMutation()
+  const [deleteTask] = useDeleteTaskMutation()
+
+  const onTaskChange = e => setTask(e.target.value)
+
+  const [deleteConfirmation, setDeleteConfirmation] = useState(false)
+
+  const onUpdateTaskClicked = async (e) => {
+    e.preventDefault()
+    await updateTask({user_id, task, id: props.item.id, stage: props.item.stage })
+
+    navigate(0)
+  }
+
+  const DeleteTask = async (e) => {
+    e.preventDefault()
+    await deleteTask({user_id, id: props.item.id})
+    navigate(0)
+  }
+
+  function DeleteTaskConfirmation() {
+    setDeleteConfirmation(!deleteConfirmation)
+  }
+
+  return (
+    <div>
+      {
+        !deleteConfirmation ? <button onClick={ DeleteTaskConfirmation } style={{...styles.delete}}>Delete</button> : <div style={styles.btnContainer}>
+          <button onClick={ DeleteTask } style={{...styles.delete}}>Yes</button>
+          <button onClick={ DeleteTaskConfirmation } style={{...styles.btn}}>No</button>
+        </div>
+      }
+      <form className="form" onSubmit={onUpdateTaskClicked}>
+        <header>
+          <h1>Update Task</h1>
+        </header>
+        <label htmlFor="task">Task</label>
+              <input
+                  className="form__input"
+                  type="text"
+                  id="task"
+                  value={task}
+                  onChange={onTaskChange}
+                  required
+              />
+        <button>Save</button>
+      </form>
+    </div>
+  )
+}
+
+let styles = {
+  start: {
+      alignSelf: 'center',
+      marginBottom: '100%'
+  },
+  link: {
+    alignSelf: 'center',
+    fontSize: '1rem',
+    textDecoration: 'none',
+    textUnderline: 'none'
+  },
+  active: {
+    borderBottom: "2px solid black",
+  },
+  delete: {
+    width: '30%',
+    padding: '0.5rem',
+    color: 'white',
+    border: 'none',
+    borderRadius: '0.5rem',
+    backgroundColor: '#ff7d77',
+    fontSize: '1.1rem',
+    cursor: 'pointer'
+  },
+  btn: {
+    width: '30%',
+    padding: '0.5rem',
+    color: 'white',
+    border: 'none',
+    borderRadius: '0.5rem',
+    backgroundColor: 'black',
+    fontSize: '1.1rem',
+    cursor: 'pointer'
+  },
+  btnContainer: {
+    width: '30%',
+    display: 'flex',
+    justifyContent: 'space-evenly'
+  }
+}
---
+++ src/components/task/taskApiSlice.js
@@ -0,0 +1,143 @@
+import {
+  createSelector, 
+  createEntityAdapter
+} from "@reduxjs/toolkit"
+
+import { apiSlice } from '../../app/api/apiSlice'
+
+// we can sort ordering here. Using sortComparaer: compare function
+const tasksAdapter = createEntityAdapter({
+
+})
+const initialState = tasksAdapter.getInitialState()
+
+export const tasksApiSlice = apiSlice.injectEndpoints({
+  endpoints: builder => ({
+    getScheduledTasks: builder.query({
+      query: (user_id) => ({url: `/tasks/${user_id}/schedule`}),
+      validateStatus: (response, result) => {
+        return response.status === 200 && !result.isError
+      },
+      transformResponse: responseData => {
+        // set frontend data from backend mongodb data
+        const loadedTasks = responseData.reverse().map(task => {
+          task.id = task._id
+          return task
+        });
+        return tasksAdapter.setAll(initialState, loadedTasks)
+      },
+      providesTags: (result, error, arg) => {
+        if(result?.ids) {
+          return [
+            { type: 'Task', id: 'LIST' },
+            ...result.ids.map(id => ({ type: 'Task', id }))
+          ]
+        } else return [{ type: 'Task', id: 'LIST' }]
+      }
+    }),
+    getProjectTasks: builder.query({
+      query: (data) => ({url: `/tasks/${data.user}/project/${data.path}`}),
+      validateStatus: (response, result) => {
+        return response.status === 200 && !result.isError
+      },
+      transformResponse: responseData => {
+        // set frontend data from backend mongodb data
+        const loadedTasks = responseData.reverse().map(task => {
+          task.id = task._id
+          return task
+        });
+        return tasksAdapter.setAll(initialState, loadedTasks)
+      },
+      providesTags: (result, error, arg) => {
+        if(result?.ids) {
+          return [
+            { type: 'Task', id: 'LIST' },
+            ...result.ids.map(id => ({ type: 'Task', id }))
+          ]
+        } else return [{ type: 'Task', id: 'LIST' }]
+      }
+    }),
+    getTasksById: builder.query({
+      query: (user_id, id) => ({url: `/tasks/${user_id}/${id}`}),
+      validateStatus: (response, result) => {
+        return response.status === 200 && !result.isError
+      },
+      transformResponse: responseData => {
+        // set frontend data from backend mongodb data
+        const loadedTasks = responseData.map(task => {
+          task.id = task._id
+          return task
+        });
+        return tasksAdapter.setAll(initialState, loadedTasks)
+      },
+      providesTags: (result, error, arg) => {
+        if(result?.ids) {
+          return [
+            { type: 'Task', id: 'LIST' },
+            ...result.ids.map(id => ({ type: 'Task', id }))
+          ]
+        } else return [{ type: 'Task', id: 'LIST' }]
+      }
+    }),
+    addNewTask: builder.mutation({
+      query: taskData => ({
+        url: `/tasks/${taskData.user_id}`,
+        method: 'POST',
+        body: {
+          ...taskData,
+        }
+      }), // force update by invalidating user list
+      invalidatesTags: [
+        { type: 'Task', id: 'LIST' }
+      ]
+    }),
+    updateTask: builder.mutation({
+      query: (taskData) => ({
+        url: `/tasks/${taskData.user_id}/${taskData.id}`,
+        method: 'PUT',
+        body: {
+          ...taskData,
+        }
+      }),
+      invalidatesTags: (result, error, arg) => [
+        { type: 'Task', id: arg.id }
+      ]
+    }),
+    deleteTask: builder.mutation({
+      query: taskData => ({
+        url: `/tasks/${taskData.user_id}/${taskData.id}`,
+        method: 'DELETE',
+        body: { ...taskData }
+      }),
+      invalidatesTags: (result, error, arg) => [
+        { type: 'Task', id: arg.id }
+      ]
+    })
+
+  }),
+})
+
+export const {
+  useGetScheduledTasksQuery,
+  useGetProjectTasksQuery,
+  useGetTasksByIdQuery,
+  useAddNewTaskMutation,
+  useUpdateTaskMutation,
+  useDeleteTaskMutation
+} = tasksApiSlice
+
+// returns the query result object
+export const selectTasksResult = tasksApiSlice.endpoints.getTasksById.select()
+
+// creates memoized selector
+const selectTasksData = createSelector(
+  selectTasksResult,
+  tasksResult => tasksResult.data // normalizewd state object wiht ids and entities
+)
+// selectAll, selectById, etc. are created automatically, we are renaming them 
+export const {
+  selectAll: selectAllTasks,
+  selectById: selectTaskById,
+  selectIds: selectTaskIds
+} = tasksAdapter.getSelectors(state => selectTasksData(state) ?? initialState) // if null, go to initialState
+
